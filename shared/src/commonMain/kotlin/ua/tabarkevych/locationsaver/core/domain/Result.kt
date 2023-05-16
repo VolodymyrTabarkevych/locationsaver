0fcf825cf3d5d241fa8389ae1c79efc0aa8b0b86
Volodymyr Tabarkevych <VolodymyrTabarkevych@gmail.com>
2023-05-16 06:39:54 +0300

    add result class

---
+++ shared/src/commonMain/kotlin/ua/tabarkevych/locationsaver/core/domain/Result.kt
@@ -0,0 +1,8 @@
+package ua.tabarkevych.locationsaver.core.domain
+
+sealed class Result<out T : Any> {
+
+    data class Success<out T : Any>(val data: T) : Result<T>()
+
+    data class Error(val exception: Exception) : Result<Nothing>()
+}
