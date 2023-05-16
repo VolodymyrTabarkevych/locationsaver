dd6323df7dd4e8b1c441b46c4da5b7c8980229d7
Volodymyr Tabarkevych <VolodymyrTabarkevych@gmail.com>
2023-05-16 05:24:43 +0300

    move theme files to core package

---
+++ androidApp/src/main/java/ua/tabarkevych/locationsaver/android/core/theme/Theme.kt
@@ -0,0 +1,22 @@
+package ua.tabarkevych.locationsaver.android.core.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val LightColorPalette = lightColors(
+    primary = Color.Black,
+    background = Color.White,
+    surface = Color.White
+)
+
+@Composable
+fun CheckoutAppTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colors = LightColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
---
+++ androidApp/src/main/java/ua/tabarkevych/locationsaver/android/core/theme/Typography.kt
@@ -0,0 +1,5 @@
+package ua.tabarkevych.locationsaver.android.core.theme
+
+import androidx.compose.material.Typography
+
+val Typography = Typography()
