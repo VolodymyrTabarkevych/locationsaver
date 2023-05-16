ad8524d68b1ebe89e8ca6d52e6bad22cc7166455
Volodymyr Tabarkevych <VolodymyrTabarkevych@gmail.com>
2023-05-16 06:41:01 +0300

    add device previews

---
+++ androidApp/src/main/java/ua/tabarkevych/locationsaver/android/core/DevicePreviews.kt
@@ -0,0 +1,11 @@
+package ua.tabarkevych.locationsaver.android.core
+
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * Multipreview annotation that represents various device sizes. Add this annotation to a composable
+ * to render various devices.
+ */
+@Preview(name = "PHONE", device = Devices.PHONE, showBackground = true)
+annotation class DevicePreviews
