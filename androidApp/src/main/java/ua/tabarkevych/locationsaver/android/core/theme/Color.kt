ea0943112290dcdd06bff72fba8a4990f3dd91cf
Volodymyr Tabarkevych <VolodymyrTabarkevych@gmail.com>
2023-05-16 05:25:41 +0300

    move theme files to core package

---
+++ androidApp/src/main/java/ua/tabarkevych/locationsaver/android/core/theme/Color.kt
@@ -0,0 +1,14 @@
+package ua.tabarkevych.locationsaver.android.core.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val Supernova = Color(0xfff5cb00)
+val QuillGray = Color(0xFFE6E5E4)
+val SilverChalice = Color(0xFFA5A5A5)
+val Sunglow = Color(0xFFFFD42F)
+val Buttermilk = Color(0xFFFFECAC)
